a431afe40e4243bb72fb273692a1606e61932e96
add7l <d93slio@outlook.com>
2024-06-09 15:38:51 +0800

    Modify air.go

--- air.go
+++ air.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Future base part likely cause chance consider.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
