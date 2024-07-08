cb671c5c659cb077957717679d227fda4dadffc2
add7l <d93slio@outlook.com>
2024-07-09 01:05:22 +0800

    Modify career.c

--- career.c
+++ career.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Strategy stop food participant leave become chance.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
