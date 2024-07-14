183830437f3b3561b2a501a3de44ce2eb3e6c4c3
add7l <d93slio@outlook.com>
2024-07-14 15:17:57 +0800

    Modify state.c

--- state.c
+++ state.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# When run trouble prepare candidate camera.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
