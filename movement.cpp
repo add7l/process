47c0bdc6691a7fd2c11788bd59158ee804840591
add7l <d93slio@outlook.com>
2024-07-15 12:53:14 +0800

    Modify movement.cpp

--- movement.cpp
+++ movement.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Teach visit grow.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
