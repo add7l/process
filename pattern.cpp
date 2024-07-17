1c94b5e1f3f9e279441dacb223adbe50d9531b90
add7l <d93slio@outlook.com>
2024-07-17 12:02:26 +0800

    Modify pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Foreign behavior box skill.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
