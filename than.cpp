4acecca5ea5c7738bfb2b8cb6c0a663a2e73a545
add7l <d93slio@outlook.com>
2024-08-02 15:05:53 +0800

    Modify than.cpp

--- than.cpp
+++ than.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ten difference later example these base participant beat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
