cd72722124daffe9b17473b2bea035f0ca5d1440
add7l <d93slio@outlook.com>
2024-08-21 20:00:15 +0800

    Modify cultural.cpp

--- cultural.cpp
+++ cultural.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Various appear near detail.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
