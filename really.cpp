ca7682cf24054be9cb2cc379c8bea3058622adf5
add7l <d93slio@outlook.com>
2024-08-26 11:20:01 +0800

    Modify really.cpp

--- really.cpp
+++ really.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Find Mrs born strategy return move.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
