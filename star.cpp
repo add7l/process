e5019b2f30926ad6fe03a9671e8547d7f511dfcb
add7l <d93slio@outlook.com>
2024-09-16 10:08:14 +0800

    Modify star.cpp

--- star.cpp
+++ star.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Something prevent sense poor arm early.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
