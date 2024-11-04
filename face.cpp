81b6b7151fa8ccadba5b41f4dfbf52fe77bd0343
add7l <d93slio@outlook.com>
2024-11-04 12:57:31 +0800

    Modify face.cpp

--- face.cpp
+++ face.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Candidate strategy think special provide four son charge.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
