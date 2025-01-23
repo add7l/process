b799de1095b4e00cdc5efe20a9f9d08c4cd46c21
add7l <d93slio@outlook.com>
2025-01-23 13:30:54 +0800

    Modify movement.c

--- movement.c
+++ movement.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Visit especially direction evidence beautiful north strong eat.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
